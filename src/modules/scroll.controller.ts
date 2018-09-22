627eb67beb7437e7713573e8ee0f4d81c24a905e
Gregor Menih <gregor@menih.si>
2018-09-22 22:08:11 +0200

    fix: make everything simpler and smoother

--- src/modules/scroll.controller.ts
+++ src/modules/scroll.controller.ts
@@ -1,74 +1,48 @@
-import {Observable, fromEvent, BehaviorSubject} from 'rxjs';
-import {tap} from 'rxjs/operators';
 import {debounce} from '../utils/function.utils';
-import {scrollSmoothly} from '../utils/scroll.utils';
+import {scrollSmoothly} from '../utils/animation.utils';
 
 export class ScrollController {
-    private $scroll: Observable<WheelEvent>;
-    
     private activePage: number = 0;
     private pageCount: number = 0;
-    private targetPosition: number = 0;
     private windowHeight: number = 0;
     private scrollHeight: number = 0;
+    private scrollPositions: number[] = [];
 
-    private isNativeScrolling: boolean = false;
-    private isAutoScrolling: boolean = false;
-    private scrollBreakpoint: number = 0.33;
-    
     constructor (private scroller: HTMLElement) {
-        this.$scroll = fromEvent<WheelEvent>(window, 'scroll');
         this.pageCount = scroller.childElementCount || 0;
         this.windowHeight = window.innerHeight;
-        this.scrollHeight = scroller.scrollHeight / this.pageCount;
         this.setBodyHeight();
+        this.getPositions();
     }
 
     public start (): void {
-        const onScrollEnd: Function = debounce(this.onScrollEnd, 80, this);
-
-        this.$scroll
-            .pipe(
-                tap((): void => {
-                    if (this.isAutoScrolling) {
-                        return;
-                    }
-                    const currentPosition: number = window.pageYOffset;
-                    const scrollOffset: number = currentPosition - this.targetPosition;
-                    const percentage: number = scrollOffset / this.windowHeight;
-                    this.scroller.scrollTop = (this.activePage * this.scrollHeight) + (this.scrollHeight * percentage);
-                    if (currentPosition >= this.targetPosition + (this.windowHeight * this.scrollBreakpoint)) {
-                        this.isAutoScrolling = true;
-                        this.changePage(1);
-
-                    } else if (currentPosition <= this.targetPosition - (this.windowHeight * this.scrollBreakpoint)) {
-                        this.isAutoScrolling = true;
-                        this.changePage(-1);
-                    }
-                }),
-                tap(() => {
-                    onScrollEnd();
-                }),
-            ).subscribe();
-    }
-
-    private onScrollEnd (): void {
-        this.isAutoScrolling = false;
-        this.isNativeScrolling = false;
-        this.scrollTo(this.targetPosition);
+        window.onscroll = () => {
+            const currentPosition: number = window.pageYOffset;
+            const percentage: number = currentPosition / this.scrollHeight;
+            const activePage: number = Math.floor(percentage * this.pageCount);
+            if (this.activePage !== activePage) {
+                this.changePage(activePage);
+            }
+        };
     }
 
     private changePage (value: number): void {
-        const newPage: number = (this.activePage += value);
-        this.targetPosition = newPage * this.windowHeight;
-        scrollSmoothly(this.scroller, newPage * this.scrollHeight, 240);
-    }
-
-    private scrollTo (position: number) {
-        window.scrollTo(0, position);
+        console.log('setting page');
+        this.activePage = value;
+        const offsetTop: number = this.scrollPositions[this.activePage];
+        scrollSmoothly(this.scroller, offsetTop, {
+            transitionTime: 240,
+        });
     }
 
     private setBodyHeight (): void {
-        document.body.style.height = `${this.windowHeight * this.pageCount}px`;
+        document.body.style.height = `${this.pageCount * this.windowHeight}px`;
+        this.scrollHeight = document.body.scrollHeight - this.windowHeight + 1;
+    }
+
+    private getPositions (): void {
+        [...this.scroller.childNodes].forEach((element: HTMLElement) => {
+            this.scrollPositions.push(element.offsetTop);
+        });
     }
 }
