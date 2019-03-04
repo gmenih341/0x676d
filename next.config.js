a75fd47aa9816270509094ce12d137c968b187cc
Gregor Menih <gregor@menih.si>
2019-03-04 13:29:45 +0100

    fix: make runnable on netlify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 const withTypescript = require('@zeit/next-typescript');
 const withTM = require('next-transpile-modules');
 
-module.exports = withTypescript(
-  withTM({
-    transpileModules: ['react-spring', '@babel/runtime']
-  })
-);
+module.exports = withTypescript();
