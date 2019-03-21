eb376d0205c7220913f946c7b8c6c9c23c02f51c
Gregor Menih <gregor@menih.si>
2019-03-21 08:36:39 +0100

    fix: button fail

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent, InputHTMLAttributes} from 'react';
-import styled from 'styled-components/macros';
+import styled from 'styled-components/macro';
 import {COLOR_MAIN, FONT_SANS, SPACER, SPACER_SMALL} from '../../style.contants';
 
 const StyledButton = styled('button')`
@@ -7,7 +7,7 @@ const StyledButton = styled('button')`
     justify-self: end;
     padding: ${SPACER_SMALL * 2}px ${SPACER * 2}px;
     border: 1px solid ${COLOR_MAIN[8]};
-    box-shadow: 2px 2px 0 ${COLOR_MAIN[9]}, -2px 2px 0 ${COLOR_MAIN[9]}, 1px 1px ${COLOR_MAIN[6]} inset, -1px 1px ${COLOR_MAIN[6]} inset;
+    box-shadow: 2px 2px 0 ${COLOR_MAIN[9]}, 1px 1px ${COLOR_MAIN[6]} inset;
     background: ${COLOR_MAIN[7]};
     color: ${COLOR_MAIN[1]};
     font-family: ${FONT_SANS};
