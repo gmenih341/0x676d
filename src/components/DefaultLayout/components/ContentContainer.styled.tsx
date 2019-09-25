c6f2fd3c51b3c030e0bc2526de4c9ab67fb77356
Gregor Menih <gregor@menih.si>
2019-09-25 22:17:08 +0200

    fix: mobile

--- src/components/DefaultLayout/components/ContentContainer.styled.tsx
+++ src/components/DefaultLayout/components/ContentContainer.styled.tsx
@@ -1,32 +1,36 @@
 import styled from 'styled-components/macro';
 import {COLOR_GRAY, SPACER_BIG, SPACER} from '../../../constants/style.constants';
+import {mediaMin} from '../../../utils/style.utils';
 
 export const ContentContainer = styled('div')`
     position: relative;
-    margin: ${-SPACER_BIG * 2}px;
-    padding: ${SPACER_BIG * 2}px;
     overflow: hidden;
 
-    &:before,
-    &:after {
-        content: '';
-        display: block;
-        position: absolute;
-        z-index: 1000;
-        top: ${SPACER_BIG}px;
-        width: ${SPACER_BIG * 2}px;
-        height: 100%;
-        transform: rotate(2deg);
-        background: ${COLOR_GRAY[3]};
-    }
+    ${mediaMin('md')} {
+        margin: ${-SPACER_BIG * 2}px;
+        padding: ${SPACER_BIG * 2}px;
 
-    &:before {
-        left: ${-SPACER}px;
-        transform-origin: top right;
-    }
+        &:before,
+        &:after {
+            content: '';
+            display: block;
+            position: absolute;
+            z-index: 1000;
+            top: ${SPACER_BIG}px;
+            width: ${SPACER_BIG * 2}px;
+            height: 100%;
+            transform: rotate(2deg);
+            background: ${COLOR_GRAY[3]};
+        }
 
-    &:after {
-        right: ${-SPACER}px;
-        transform-origin: bottom left;
+        &:before {
+            left: ${-SPACER}px;
+            transform-origin: top right;
+        }
+
+        &:after {
+            right: ${-SPACER}px;
+            transform-origin: bottom left;
+        }
     }
 `;
